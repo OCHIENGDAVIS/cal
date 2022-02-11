d82db64166f37622ed17259529d93e35ea6d4b13
davis <davisochieng4@gmail.com>
2022-02-10 23:01:09 -0500

    Added support for double addition to the Calculator.java class

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,9 @@ public class Calculator{
   public static void main(string args[]){
     public int addition(int a, int b){
       return a + b
-
+    }
+    public double addition(int a, int b){
+      return a + b
     }
 
   }
