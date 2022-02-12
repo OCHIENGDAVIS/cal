d999267c9c9c61fa64d18ba2d72b50474e06cae3
davis <davisochieng4@gmail.com>
2022-02-11 20:43:01 -0500

    added conflict through a comment

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,14 @@
 public class Calculator{
   public static void main(string args[]){
+
     public int addition(int a, int b){
       return b + a
 
+    }
+
+    public doulble addition(double a, double b){
+      // causes merge conflict
+      return b + a
     }
 
   }
